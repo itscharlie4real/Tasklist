2cab7f4a92c23ac8b75302eebb9c8d2b23147cdb
Charlie Migwi <charliemigwi@gmail.com>
2019-12-06 15:26:41 +0300

    add filter event listener and function

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners() {
   taskList.addEventListener("click", removeTask);
   // Clear tasks using the clear button
   clearBtn.addEventListener("click", clearTasks);
+  // Filter tasks event
+  filter.addEventListener("keyup", filterTasks);
 }
 
 //Add task
@@ -60,8 +62,22 @@ function removeTask(e) {
 //Clear Tasks
 function clearTasks() {
   // taskList.innerHTML = "";
+
   //Alternative method to clear tasks
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
+}
+
+//Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll(".collection-item").forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
